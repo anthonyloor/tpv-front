c066c78a842dd09d67b542df7201c0b77d3deb38
Anthony Loor <111461610+anthonyloor@users.noreply.github.com>
2025-02-09 15:44:49 +0100

    refactorizar index.js: agregar MantineProvider para mejorar la gestión de estilos y la presentación de la aplicación

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import AuthProvider from './contexts/AuthContext';
-import { ConfigProvider } from './contexts/ConfigContext';
-import { ClientProvider } from './contexts/ClientContext';
-import { BrowserRouter as Router } from 'react-router-dom';
-import PinProvider from './contexts/PinContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { MantineProvider } from "@mantine/core";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AuthProvider from "./contexts/AuthContext";
+import { ConfigProvider } from "./contexts/ConfigContext";
+import { ClientProvider } from "./contexts/ClientContext";
+import { BrowserRouter as Router } from "react-router-dom";
+import PinProvider from "./contexts/PinContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <Router>
         <ConfigProvider>
-          <ClientProvider>
-            <PinProvider>
-              <App />
-            </PinProvider>
-          </ClientProvider>
+          <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+              colorScheme: "light", // o 'dark'
+              // puedes personalizar colores, tipografías, radius, etc.
+            }}
+          >
+            <ClientProvider>
+              <PinProvider>
+                <App />
+              </PinProvider>
+            </ClientProvider>
+          </MantineProvider>
         </ConfigProvider>
       </Router>
     </AuthProvider>
